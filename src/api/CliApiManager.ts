528b68fc41ba38cc5888f20089c33ef0daf2225a
Kasra Bigdeli <kasra85@gmail.com>
2019-01-14 21:15:40 -0800

    Updated API Version to v2

--- src/api/CliApiManager.ts
+++ src/api/CliApiManager.ts
@@ -22,7 +22,7 @@ export default class CliApiManager {
 	static get(capMachine: IMachine) {
 		const hashKey = 'v' + hashCode(capMachine.baseUrl);
 		if (!CliApiManager.instances[hashKey])
-			CliApiManager.instances[hashKey] = new ApiManager(capMachine.baseUrl + '/api/v1', function(token) {
+			CliApiManager.instances[hashKey] = new ApiManager(capMachine.baseUrl + '/api/v2', function(token) {
 				capMachine.authToken = token;
 				if (capMachine.name) StorageHelper.get().saveMachine(capMachine);
 				return Promise.resolve();
