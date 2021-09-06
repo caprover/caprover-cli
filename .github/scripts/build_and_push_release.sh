993863c37b2239b73c9d78a18a5c0742c17357c5
Kasra Bigdeli <kasra85@gmail.com>
2021-09-06 16:34:40 -0700

    Updated github workflows

--- .github/scripts/build_and_push_release.sh
+++ .github/scripts/build_and_push_release.sh
@@ -48,15 +48,6 @@ else
 fi
 
 
-# BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# On Github the line above does not work, instead:
-BRANCH=${GITHUB_REF##*/}
-echo "on branch $BRANCH"
-if [[ "$BRANCH" != "release" ]]; then
-    echo 'Not on release branch! Aborting script!';
-    exit 1;
-fi
-
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
