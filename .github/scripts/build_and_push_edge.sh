c6aaa761c505834a89dbd2d3894ab55f066d96d2
Kasra Bigdeli <kasra85@gmail.com>
2026-08-28 22:17:01 -0700

    Drop ARMv7 from edge image build

--- .github/scripts/build_and_push_edge.sh
+++ .github/scripts/build_and_push_edge.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Print all commands
-set -x 
+set -x
 
 pwd
 
@@ -16,34 +16,26 @@ else
     echo "Running on CI"
 fi
 
-
 CAPROVER_VERSION=edge
 IMAGE_NAME=caprover/cli-caprover
 
 if [ ! -f ./package-lock.json ]; then
     echo "package-lock.json not found!"
-    exit 1;
+    exit 1
 fi
-
 
 # BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # On Github the line above does not work, instead:
 BRANCH=${GITHUB_REF##*/}
 echo "on branch $BRANCH"
 if [[ "$BRANCH" != "master" ]]; then
-    echo 'Not on master branch! Aborting script!';
-    exit 1;
+    echo 'Not on master branch! Aborting script!'
+    exit 1
 fi
 
-
-
-
-
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx ls
-docker buildx create --name mybuilder
-docker buildx use mybuilder
-
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm -t $IMAGE_NAME:$CAPROVER_VERSION -f Dockerfile --push .
-
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    -t "$IMAGE_NAME:$CAPROVER_VERSION" \
+    -f Dockerfile \
+    --push \
+    .
